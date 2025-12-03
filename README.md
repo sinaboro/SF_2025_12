eb15085e1f3a8d723da26775753a350e757814fb
kimdaechul <rlaeocjf@gmail.com>
2025-12-03 18:17:22 +0900

    message

--- README.md
+++ README.md
@@ -8,3 +8,14 @@ CREATE DATABASE springdb;
 CREATE USER 'springdbuser'@'%' IDENTIFIED BY '1234';
 
 GRANT ALL PRIVILEGES ON springdb.* TO 'springdbuser'@'%';
+
+'''sql
+create table table_board(
+	bno int auto_increment primary,
+    title varchar(500) not null,
+    content varchar(2000) not null,
+    writer varchar(50) not null,
+    regdate timestamp default now(),
+    updatedate timestamp default now(),
+    delflag boolean default false
+);
--- sp1/src/main/java/org/zerock/controller/HelloController.java
+++ sp1/src/main/java/org/zerock/controller/HelloController.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
@@ -65,6 +66,14 @@ public class HelloController {
 	@GetMapping("/ex4")
 	public void ex4(@RequestParam(name="n1", defaultValue = "1") int num,
 					@RequestParam("name") String name) {
+		
+		log.info("num : " + num);
+		log.info("name : " + name);		
+	}
+
+	@PostMapping("/ex4")
+	public void ex4Post(@RequestParam(name="n1", defaultValue = "1") int num,
+			@RequestParam("name") String name) {
 		
 		log.info("num : " + num);
 		log.info("name : " + name);		
