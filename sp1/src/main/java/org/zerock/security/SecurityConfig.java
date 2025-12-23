6f86d62512372e124a8aa1cc7f97f9721ef7aa8c
kimdaechul <rlaeocjf@gmail.com>
2025-12-23 13:01:56 +0900

    message

---
+++ sp1/src/main/java/org/zerock/security/SecurityConfig.java
@@ -0,0 +1,23 @@
+package org.zerock.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+import lombok.extern.log4j.Log4j2;
+
+@Configuration
+@Log4j2
+@EnableWebSecurity
+public class SecurityConfig {
+
+	@Bean
+	public SecurityFilterChain filterChain(HttpSecurity http) throws Exception{
+		
+		log.info("---------------filterChain----------------------");
+		
+		return http.build();
+	}
+}
