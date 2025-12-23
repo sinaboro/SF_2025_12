ce25434ef1e85f17a900d1fef144223382192486
kimdaechul <rlaeocjf@gmail.com>
2025-12-23 14:54:15 +0900

    message

---
+++ sp1/src/main/java/org/zerock/security/CustomUserDetailsService.java
@@ -0,0 +1,46 @@
+package org.zerock.security;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.AuthenticationUserDetailsService;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+import lombok.extern.log4j.Log4j2;
+
+@Service
+@Log4j2
+public class CustomUserDetailsService implements UserDetailsService{
+	
+	@Autowired
+	private PasswordEncoder encoder; 
+	
+	@Override	
+	public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+		
+		log.info("-----------------loadUserByUsername----------------------");
+		log.info("username : " + username);
+		
+		log.info("encoder : " + encoder);
+		/*
+		UserDetails user = User.builder()
+				.username(username)
+				.password("{noop}1111")  -> {noop} 비밀번호 암호화 X 
+				.roles("USER")
+				.build();
+		*/
+		
+		UserDetails user = User.builder()
+				.username(username)
+				.password(encoder.encode("1111"))
+				.roles("USER")
+				.build();
+		
+		return user;
+	}
+
+}
